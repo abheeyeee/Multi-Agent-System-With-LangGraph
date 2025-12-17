70fe998a4720c6b1286240a8ac61b75378fb10ef
REXSY <abhinavsingh.thedev@gmail.com>
2025-12-17 13:29:11 +0530

    defining the workflow

---
+++ graph_builder.py
@@ -0,0 +1,15 @@
+from langgraph.graph import StateGraph, END
+from state import AgentState
+from agents import researcher_node, writer_node
+
+workflow = StateGraph(AgentState)
+
+workflow.add_node("Researcher", researcher_node)
+workflow.add_node("Writer", writer_node)
+
+workflow.set_entry_point("Researcher")
+workflow.add_edge("Researcher", "Writer")
+workflow.add_edge("Writer", END)
+
+compiled_graph = workflow.compile()
+
